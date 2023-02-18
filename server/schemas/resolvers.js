53660fc54ec3c312ef52edb4081d035e80ea1324
StevenM <stevenbmendoza@gmail.com>
2023-02-17 21:25:45 -0800

    begin format of GQL to MongoDB Queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,18 @@
+const { User, Thought } = require("../models");
+
 // these resolvers contain menthods with the names as the query or mutation
+
+// pass in parent as a placeholder parameter for first parameter's spot to
+// access the username argument from the second parameter. Use a ternary operator
+// to check if username exists. If yes, set params to an object with username key
+// If not, return an empty object. Pass that object, with or without any data in it
+// , .find() method. Data? , perform a lookup by a specific username. If there's not,
+//  return every thought.
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,9 +4,32 @@ const { gql } = require(`apollo-server-express`);
 
 // create our typeDefs, define query with type Query {} named "helloWorld" with
 // type. Set up resolvers in js file.
+
+// we're instructing our query that we'll return an array, as noted by the []
+// square brackets around the returning data, Thought.
+
+// defined our thoughts query that it could receive a parameter if we wanted.
+// In this case, the parameter would be identified as username and would have
+// a String data type. Query with or without username param
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
   }
 `;
 
