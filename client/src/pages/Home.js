3ee7e2c2b4374d30aa08cb0bba30c63ff172cb8b
StevenM <stevenbmendoza@gmail.com>
2023-02-20 21:03:13 -0800

    add thoughtList component to Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,8 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_THOUGHTS } from "../utils/queries";
 
+import ThoughtList from "../components/ThoughtList";
+
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
@@ -15,12 +17,21 @@ const Home = () => {
   // it is undefined. If data exists, store it in the thoughts constant we just created.
   // If data is undefined, then save an empty array to the thoughts component.
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+  // console.log(thoughts);
 
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
       </div>
     </main>
   );
