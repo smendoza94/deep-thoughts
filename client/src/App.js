0166f78f13e9e0ee46fc5acc477e2459f824197e
StevenM <stevenbmendoza@gmail.com>
2023-02-20 17:48:35 -0800

    create apollo endpoint and access to JSX

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
+// ApolloProvider is a special type of React component that we'll use to provide data to all
+// of the other components. ApolloClient is a constructor function that will help initialize
+// the connection to the GraphQL API server. InMemoryCache enables the Apollo Client instance
+// to cache API response data so that we can perform requests more efficiently. createHttpLink
+// allows us to control how the Apollo Client makes a request. Think of it like middleware for
+// the outbound network requests.
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+// establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink().
+const httpLink = createHttpLink({ uri: "http://localhost:3001/graphql" });
+
+// ApolloClient() constructor to instantiate the Apollo Client instance and create the connection
+// to the API endpoint. We also instantiate a new cache object using new InMemoryCache().
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// wrap the entire returning JSX code with <ApolloProvider>. Because we're passing the client
+// variable in as the value for the client prop in the provider, everything between the JSX tags
+// will eventually have access to the server's API data through the client we set up.
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
