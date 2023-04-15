224347c8d0f545df1e417884f58eebd10821dbc0
StevenM <stevenbmendoza@gmail.com>
2023-04-14 19:28:36 -0700

    gql login mutation added to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
+    // Remember that the ... in this context is being used as the spread operator.
+    // This means that we are setting the variables field in our mutation to be an
+    // object with key/value pairs that match directly to what our formState object
+    // looks like.
     setFormState({
       ...formState,
       [name]: value,
@@ -17,42 +25,56 @@ const Login = (props) => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-md-6">
+        <div className="card">
+          <h4 className="card-header">Login</h4>
+          <div className="card-body">
             <form onSubmit={handleFormSubmit}>
               <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
                 value={formState.email}
                 onChange={handleChange}
               />
               <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
+              <button className="btn d-block w-100" type="submit">
                 Submit
               </button>
             </form>
+            {error && (
+              <div>
+                <p className="text-danger">{error.message}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
