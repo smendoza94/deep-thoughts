65cf58906161eb0bc92f253adc33ba9844ce4126
StevenM <stevenbmendoza@gmail.com>
2023-04-17 21:36:47 -0700

    logout functionality

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 
+const logout = (event) => {
+  event.preventDefault();
+  Auth.logout();
+};
+
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
@@ -18,7 +23,9 @@ const Header = () => {
           {Auth.loggedIn() ? (
             <>
               <Link to="/profile">Me</Link>
-              <a href="/">Logout</a>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
             </>
           ) : (
             <>
