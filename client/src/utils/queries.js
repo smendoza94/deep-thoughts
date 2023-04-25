6ab9431cb300422c9e6875666c1ef7baac11234c
StevenM <stevenbmendoza@gmail.com>
2023-04-25 14:01:04 -0700

    two queries for loggin in self data

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -56,3 +56,50 @@ export const QUERY_USER = gql`
     }
   }
 `;
+
+// retrieve essentially all data related to the logged-in user. We'll retrieve their
+// user information, thoughts, reactions to those thoughts, and friend list. This
+// one will be great for the user's personal profile page
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// requesting significantly less data to be returned over HTTP.
+// With GraphQL, we can reuse the same query we created and simply ask for less
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
