7dcdfd148ee94e1e0dec73bcb0748dcea20b6175
StevenM <stevenbmendoza@gmail.com>
2023-05-03 22:04:56 -0700

    user profile page after auth, visit other user page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,44 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import ThoughtList from "../components/ThoughtList";
 import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import FriendList from "../components/FriendList";
+import Auth from "../utils/auth";
 
 const Profile = () => {
-  const { username: useParam } = useParams();
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: useParam },
+  const { username: userParam } = useParams();
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
   });
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+  // redirect to personal profile page if username is the logged-in user's
+  // we're checking to see if the user is logged in and if so, if the username
+  // stored in the JSON Web Token is the same as the userParam value. If they
+  // match, we return the <Navigate> component with the prop to set to the value
+  // /profile, which will redirect the user away from this URL and to the /profile
+  // route.
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
   if (loading) {
     return <div>Loading...</div>;
+  }
+  // if there is no user data to display, we know that we aren't logged in or at
+  // another user's profile page, therefore notify that they must sign up or log in
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
+      </h4>
+    );
   }
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
       </div>
 
