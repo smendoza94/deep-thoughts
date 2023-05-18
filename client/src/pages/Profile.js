30151f55b107adb2e6b3e8ca4f127f40cb0a4982
StevenM <stevenbmendoza@gmail.com>
2023-05-17 20:30:13 -0700

    added add friend button, error/bugged

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Navigate, useParams } from "react-router-dom";
 import ThoughtList from "../components/ThoughtList";
-import { useQuery } from "@apollo/client";
+import { ADD_FRIEND } from "../utils/mutations";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import FriendList from "../components/FriendList";
 import Auth from "../utils/auth";
@@ -18,6 +19,16 @@ const Profile = () => {
   // match, we return the <Navigate> component with the prop to set to the value
   // /profile, which will redirect the user away from this URL and to the /profile
   // route.
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Navigate to="/profile" />;
   }
@@ -40,6 +51,11 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
